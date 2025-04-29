59eb43919878f1a4704144fe230525a8ba2f852a
keulma <jjyoung0403@naver.com>
2025-04-29 22:37:06 +0900

    cleanup

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,25 +1,20 @@
 import {useState, useEffect} from "react";
 
+function Hello(){
+  useEffect(() => {
+    console.log("print Hello")
+    return () => console.log("destroy Hello");
+  }, [])
+  return<h1>Hello</h1>;
+}
+
 function App(){
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("")
-    const onClick = () => setValue((prev) => prev + 1)
-    const onChange = (event) => setKeyword(event.target.value);
-    console.log("I run all the time");
-    useEffect(() => {
-        console.log("CALL THE API...")
-    }, []);
-    
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
     return(
         <div>
-          <input 
-            value = {keyword}
-            onChange = {onChange}
-            type = "text"
-            placeholder = "Search here..."
-          />
-          <h1>{counter}</h1>
-          <button onClick = {onClick}>click me</button>
+          {showing ? <Hello /> : null}
+          <button onClick = {onClick}>{showing ? "Hide" : "Show"}</button>
         </div>
     )
 }
