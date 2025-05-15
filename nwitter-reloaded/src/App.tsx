9133bed614c11e1d9f96474a70c067c8384eb47d
keulma <jjyoung0403@naver.com>
2025-05-15 17:32:35 +0900

    firebase initialize

--- nwitter-reloaded/src/App.tsx
+++ nwitter-reloaded/src/App.tsx
@@ -1,11 +1,13 @@
+import { useState, useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/layout";
-import Home from "./routes/home";
 import Profile from "./routes/profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
+import LoadingScreen from "./components/loading-screen";
 import { createGlobalStyle } from "styled-components";
 import { reset } from "styled-reset";
+import Home from "./routes/home";
 const router = createBrowserRouter([
   {
     path :"/",
@@ -13,7 +15,7 @@ const router = createBrowserRouter([
     children :[
       {
         path : "",
-        element : <Home />,
+        element :<Home/>,
       },
       {
         path : "profile",
@@ -44,10 +46,18 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  
+  const init = async() =>{
+    setLoading(false);
+  }
+  useEffect(() => {
+    init();
+  })
   return (
     <>
       <GlobalStyles />
-      <RouterProvider router = {router} />
+      {isLoading ? <LoadingScreen /> :  <RouterProvider router = {router} />}
     </>
   )
 }
---
+++ nwitter-reloaded/src/firbase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZB8zCWyt_TP7JAR9UPLFseVKGI0Q8ha8",
+  authDomain: "nwitter-reloaded-ebd0f.firebaseapp.com",
+  projectId: "nwitter-reloaded-ebd0f",
+  storageBucket: "nwitter-reloaded-ebd0f.firebasestorage.app",
+  messagingSenderId: "470303544339",
+  appId: "1:470303544339:web:09d965e490ae3bcf815be6",
+  measurementId: "G-1VR5JYS4QN"
+};
+
+const app = initializeApp(firebaseConfig);
